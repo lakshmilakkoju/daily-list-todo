7220066e2e8d893c629ed46b0bd42ae8bbc17c1a
lakshmilakkoju <124881818+lakshmilakkoju@users.noreply.github.com>
2023-10-18 19:10:36 +0530

    Update TodoItem.js

--- src/components/TodoItem.js
+++ src/components/TodoItem.js
@@ -1,1 +1,25 @@
+import './index.css'
+
+const TodoItem = props => {
+  const {todoList, updateListFunction} = props
+  const deleteFunction = eqId => {
+    updateListFunction(eqId)
+  }
+
+  return (
+    <li className="css-li-container">
+      <p>{todoList.title}</p>
+
+      <button
+        type="button"
+        className="css-button-itself"
+        onClick={() => deleteFunction(todoList.id)}
+      >
+        Delete
+      </button>
+    </li>
+  )
+}
+
+export default TodoItem
 
